d0c9598bff6aa64a1f3bdee7b34c12c3ee5f9795
moonshotdigitals <asim.moonshot@gmail.com>
2025-06-24 15:58:19 +0500

    1.0.4

--- test.php
+++ test.php
@@ -9,14 +9,14 @@
  * that starts the plugin.
  *
  * @link              https://abc.com
- * @since             1.0.3
+ * @since             1.0.4
  * @package           Test
  *
  * @wordpress-plugin
  * Plugin Name:       Test
  * Plugin URI:        https://abc.com
  * Description:       testing deployment.
- * Version:           1.0.3
+ * Version:           1.0.4
  * Author:            Test
  * Author URI:        https://abc.com/
  * License:           GPL-2.0+
@@ -32,10 +32,10 @@ if ( ! defined( 'WPINC' ) ) {
 
 /**
  * Currently plugin version.
- * Start at version 1.0.3 and use SemVer - https://semver.org
+ * Start at version 1.0.4 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'TEST_VERSION', '1.0.3' );
+define( 'TEST_VERSION', '1.0.4' );
 
 /**
  * The code that runs during plugin activation.
@@ -95,7 +95,7 @@ add_filter('auto_update_plugin', function ($update, $item) {
  * then kicking off the plugin from this point in the file does
  * not affect the page life cycle.
  *
- * @since    1.0.3
+ * @since    1.0.4
  */
 function run_test() {
 
